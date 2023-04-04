c08c414ffa12eaa9bc441e32f00214054d2bbefd
lior baruchovich <62611078+liorBaru@users.noreply.github.com>
2023-04-04 20:23:31 +0300

    Add files via upload

---
+++ effect.js
@@ -0,0 +1,7 @@
+$(".mainMyimg").mouseover(function(){
+  $(".mainMyimg").attr("src", "images/colored.png");
+})
+
+$(".top-container_rest").mouseover(function(){
+  $(".mainMyimg").attr("src", "images/blackWh.png");
+})
